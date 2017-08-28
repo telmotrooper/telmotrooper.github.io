28de5b943d72e07cecebe9d963212f1547d1f037
Telmo "Trooper <telmo.trooper@gmail.com>
2017-08-28 09:59:18 -0300

    New page

---
+++ scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$("#name").clone().appendTo("#phone-bio");
+	$("#mail").clone().appendTo("#phone-bio");
+	$("#occupation").clone().appendTo("#phone-bio");
+	$("#location").clone().appendTo("#phone-bio");
+  });
